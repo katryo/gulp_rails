42adf9a4a07d488884839206a90f742eaadb0b7d
Ryo Kato <ryo.a.kato@dena.com>
2014-10-29 18:12:56 +0900

    gulp-util added

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require("gulp");
+var gutil = require("gulp-util");
+var webpack = require("webpack");
+var WebpackDevServer = require("webpack-dev-server");
+
+gulp.task("webpack", function(callback) {
+    // run webpack
+    webpack({
+        // configuration
+    }, function(err, stats) {
+        if(err) throw new gutil.PluginError("webpack", err);
+        gutil.log("[webpack]", stats.toString({
+            // output options
+        }));
+        callback();
+    });
+});
